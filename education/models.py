92f866e85a348ca78b52e7497c54728fd1582e12
Islam <islamka.1994@mail.ru>
2024-05-20 06:47:48 +0300

    fix config, add 2 apps(users, education)

---
+++ education/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='education/image', **NULLABLE, verbose_name='превью')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    lesson = models.ForeignKey(Course, related_name='courses', on_delete=models.SET_NULL, **NULLABLE, default=None,
+                               verbose_name='урок')
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='education/image', **NULLABLE, verbose_name='превью')
+    description = models.TextField(verbose_name='описание')
+    video_link = models.URLField(**NULLABLE, verbose_name='ссылка на видео')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+
---
+++ users/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    first_name = models.CharField(
+        max_length=100, verbose_name="Имя", help_text="Укажите Имя"
+    )
+    last_name = models.CharField(
+        max_length=100, verbose_name="Фамилия", help_text="Укажите Фамилию"
+    )
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=50, **NULLABLE, verbose_name="Телефон", help_text="Укажите телефон"
+    )
+    city = models.CharField(
+        max_length=100, **NULLABLE, verbose_name="Город", help_text="Укажите город"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar",
+        **NULLABLE,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
